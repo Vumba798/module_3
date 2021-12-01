34200ce18336ca8e4b154be4b2e5131060b23f0f
Vumba798 <alexandrov32649@gmail.com>
2021-12-01 15:25:29 +0300

    added tests for KnapsackSolver

--- src/test/kotlin/test.kt
+++ src/test/kotlin/test.kt
@@ -4,6 +4,28 @@ import java.util.*
 import kotlin.test.assertEquals
 
 class Test {
+    fun testKnapSackSolver(number: Int) {
+        val scanner = Scanner(File("tests/KnapsackSolver/input/$number.txt"))
+        val capacity = scanner.nextInt()
+        val solver = KnapsackSolver(capacity)
+        val writer = File("tests/KnapsackSolver/myOutput/$number.txt").bufferedWriter().use {
+            try {
+                while (scanner.hasNextInt()) {
+                    val weight = scanner.nextInt()
+                    val cost = scanner.nextInt()
+                    solver.insert(weight, cost)
+                }
+                val answer = solver.calculate()
+                it.write("${answer.totalWeight} ${answer.totalCost}\n")
+                for (index in answer.indexes) {
+                    it.write(index.toString())
+                    it.write("\n")
+                }
+            } catch (exception: Exception) {
+                println("An error has occurred: ${exception.message}")
+            }
+        }
+    }
     fun testCrazyRich(number: Int) {
         val scanner = Scanner(File("tests/CrazyRich/input/$number.txt"))
         val money = scanner.nextLong()
@@ -22,11 +44,11 @@ class Test {
 
     @Test
     fun testOutput() {
-        for (i in 1..102) {
+        for (i in 1..16) {
             println("test #$i")
-            testCrazyRich(i)
-            val sc1 = Scanner(File("tests/CrazyRich/myOutput/$i.txt"))
-            val sc2 = Scanner(File("tests/CrazyRich/output/$i.txt"))
+            testKnapSackSolver(i)
+            val sc1 = Scanner(File("tests/KnapsackSolver/myOutput/$i.txt"))
+            val sc2 = Scanner(File("tests/KnapsackSolver/output/$i.txt"))
             var numberOfLine = 1
             while (sc2.hasNext()) {
                 val myOutput = sc1.nextLine()
